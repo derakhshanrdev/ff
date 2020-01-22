b6ed541b7b4085a35fa419f8009ee948eac2ac68
derakhshan <derakhshanrdev@yahoo.com>
2020-01-22 09:10:10 +0330

    class and function +, /

--- cls/continue_class.py
+++ cls/continue_class.py
@@ -2,15 +2,8 @@ class my_class:
     def __init__(self, *args):
         self.args = args
 
-    def __add__(self, other):
-        a = self.args + self.other
-        return a
-
-    def __str__(self):
-        return '{0} + {1} = {2}'.format()
+    def __repr__(self):
+        return '{}'.format((self.args))
 
 
-Value1 = mycls("Red", "Green", "Blue")
-Value2 = mycls("Yellow", "Purple", "Cyan")
-Value3 = Value1 + Value2
-print("{0} + {1} = {2}".format(Value1, Value2, Value3))
+print(my_class())
